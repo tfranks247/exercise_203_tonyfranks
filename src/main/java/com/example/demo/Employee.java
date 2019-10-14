800a1265d8ab93f8ad32c1194279787fdf47899c
TPCF241005UR <TPCF241005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-14 13:51:15 -0400

    first push

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int ssn;
+    private int dob;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+}
